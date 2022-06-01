763b6a1c70843ddffe44a5090ce79ead8a2ac5bd
Tyler Knapp <knapptr@gmail.com>
2022-06-01 18:38:49 -0400

    Adds styling

--- frontend/src/pages/CreateSchedule.js
+++ frontend/src/pages/CreateSchedule.js
@@ -1,18 +1,26 @@
 import { Link, Outlet, useParams } from "react-router-dom";
 import useGetDataOnMount from "../hooks/useGetData";
 import tw, { styled } from "twin.macro";
+import "styled-components/macro";
 import { useState } from "react";
 import SelectActivities from "../components/SelectActivities";
 
-const Days = tw.ul` flex`;
+const Days = tw.ul` flex justify-center`;
 const Day = styled.li(({ isSelected }) => [
-	tw`m-2 p-2`,
+	tw`p-2`,
 	isSelected && tw`bg-red-200`,
 ]);
-const Periods = tw.ul`flex`;
+const Periods = tw.ul`flex justify-center`;
 const Period = styled.li(({ isSelected }) => [
-	tw`m-2 p-2`,
+	tw` p-2`,
 	isSelected && tw`bg-green-200`,
+]);
+const CabinNav = styled.nav(({ isOpen }) => [
+	tw`hidden md:block`,
+	isOpen && tw`block`,
+]);
+const CabinLink = styled.li(({ isSelected }) => [
+	isSelected && tw`bg-purple-200`,
 ]);
 
 const Activities = tw.ul`flex`;
@@ -27,15 +35,52 @@ const CreateSchedulePage = () => {
 		runOn: [weekNumber, cabin],
 		initialState: {},
 	});
+	const [cabins, setCabins] = useGetDataOnMount({
+		url: `/api/cabin-sessions?week=${weekNumber}`,
+		initialState: [],
+		optionalSortFunction: (cab) => {
+			if (cab.cabinArea === "GA") {
+				return -1;
+			} else {
+				return 1;
+			}
+		},
+	});
 	//currently sending a request that 500s on mount...fix
 	const [activities, setActivities] = useGetDataOnMount({
 		url: `/api/activities?period=${selectedPeriod}`,
 		runOn: [selectedPeriod],
 		initialState: [],
 	});
+	const [showCabinNav, setShowCabinNav] = useState(false);
 	return (
-		<>
-			<Days tw="text-blue-50">
+		<div tw="flex flex-col justify-center">
+			<button
+				onClick={() => setShowCabinNav((s) => !s)}
+				tw="block md:hidden"
+			>
+				{showCabinNav ? "Hide Cabins" : "Show Cabins"}
+			</button>
+			<CabinNav isOpen={showCabinNav}>
+				<ul tw="flex justify-center flex-wrap">
+					{cabins.map((cabinLink, index) => {
+						return (
+							<CabinLink
+								key={`cabin-link-${index}`}
+								isSelected={cabinLink.cabinName === cabin}
+								tw="mx-2 px-2 py-1 rounded"
+							>
+								<Link
+									to={`../sign-up/${cabinLink.cabinName}/${weekNumber}`}
+								>
+									{cabinLink.cabinName}
+								</Link>
+							</CabinLink>
+						);
+					})}
+				</ul>
+			</CabinNav>
+			<Days>
 				{week.days &&
 					week.days.map((day, index) => (
 						<Day
@@ -47,7 +92,6 @@ const CreateSchedulePage = () => {
 							}}
 						>
 							{day.dayName}
-							{day.dayID}
 						</Day>
 					))}
 			</Days>
@@ -71,7 +115,7 @@ const CreateSchedulePage = () => {
 			{selectedPeriod && (
 				<SelectActivities cabinName={cabin} periodID={selectedPeriod} />
 			)}
-		</>
+		</div>
 	);
 };
 
