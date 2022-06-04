40b2a52de52fd7ba1a3de513837223ebd6286105
Tyler Knapp <knapptr@gmail.com>
2022-06-03 20:43:31 -0400

    Adds loading to sign-up index

--- frontend/src/pages/SignUpIndex.js
+++ frontend/src/pages/SignUpIndex.js
@@ -5,6 +5,7 @@ import tw, { styled } from "twin.macro";
 import toTitleCase from "../toTitleCase";
 import "styled-components/macro";
 import { PopOut, MenuSelector } from "../components/styled";
+import cl from "../cl.png";
 
 const SignUpIndex = () => {
 	const navigate = useNavigate();
@@ -34,22 +35,23 @@ const SignUpIndex = () => {
 	};
 	return (
 		<>
-			<header tw="my-2">
-				<h1>Select Week</h1>
-			</header>
-			{selected.cabin && selected.week && (
-				<PopOut
-					shouldDisplay={true}
-					onClick={() => {
-						setSelected({
-							cabin: undefined,
-							week: undefined,
-						});
-					}}
-				>
-					<div tw=" flex flex-col justify-center border bg-stone-300 max-w-md shadow rounded-lg px-4 py-6 w-10/12">
-						<button
-							tw="self-end mr-2"
+			{!(weeks.length > 0 && cabins.length > 0) ? (
+				<>
+					<p>loading...</p>
+					<img
+						tw="animate-pulse w-1/2 mx-auto"
+						src={cl}
+						alt="camp leslie"
+					/>
+				</>
+			) : (
+				<>
+					<header tw="my-2">
+						<h1>Select Week</h1>
+					</header>
+					{selected.cabin && selected.week && (
+						<PopOut
+							shouldDisplay={true}
 							onClick={() => {
 								setSelected({
 									cabin: undefined,
@@ -57,53 +59,65 @@ const SignUpIndex = () => {
 								});
 							}}
 						>
-							X
-						</button>
-						<header tw="text-xl">
-							<h1 tw="text-base">Sign-Up</h1>
-							<h2>Cabin {toTitleCase(selected.cabin)}</h2>
-							<p tw="text-base font-light">for</p>{" "}
-							<h2>Week {selected.week}?</h2>
-						</header>
-						<div tw="flex justify-center w-10/12 mx-auto mt-3">
-							<Link
-								tw="bg-green-300 rounded border border-stone-800 py-1 px-6"
-								to={`${selected.cabin}/${selected.week}`}
+							<div tw=" relative flex flex-col justify-center border bg-stone-300 max-w-md shadow-2xl rounded-lg px-4 py-2 w-10/12">
+								<button
+									tw="self-end mr-2 absolute top-0 text-gray-500"
+									onClick={() => {
+										setSelected({
+											cabin: undefined,
+											week: undefined,
+										});
+									}}
+								>
+									X
+								</button>
+								<header tw="text-xl">
+									<h1 tw="text-base">Sign-Up</h1>
+									<h2>Cabin {toTitleCase(selected.cabin)}</h2>
+									<p tw="text-base font-light">for</p>{" "}
+									<h2>Week {selected.week}?</h2>
+								</header>
+								<div tw="flex justify-center w-10/12 mx-auto mt-3">
+									<Link
+										tw="bg-green-300 rounded border border-stone-800 py-1 px-6"
+										to={`${selected.cabin}/${selected.week}`}
+									>
+										Go!
+									</Link>
+								</div>
+							</div>
+						</PopOut>
+					)}
+					<ul tw="flex gap-1 flex-wrap flex-col">
+						{weeks.map((w) => (
+							<MenuSelector
+								tw="p-1 border  cursor-pointer"
+								isSelected={w.number === selected.week}
+								onClick={() => {
+									select("week", w.number);
+								}}
 							>
-								Go!
-							</Link>
-						</div>
-					</div>
-				</PopOut>
+								<p>Week {w.number}</p>
+								<p tw="italic font-thin">{w.title}</p>
+							</MenuSelector>
+						))}
+					</ul>
+					<header>
+						<h1>Select Cabin</h1>
+					</header>
+					<ul tw="flex flex-wrap gap-1 justify-center">
+						{cabins.map((c) => (
+							<MenuSelector
+								tw="p-2 border w-1/5"
+								isSelected={c.name === selected.cabin}
+								onClick={() => select("cabin", c.name)}
+							>
+								{c.name}
+							</MenuSelector>
+						))}
+					</ul>
+				</>
 			)}
-			<ul tw="flex gap-1 flex-wrap flex-col">
-				{weeks.map((w) => (
-					<MenuSelector
-						tw="p-1 border  cursor-pointer"
-						isSelected={w.number === selected.week}
-						onClick={() => {
-							select("week", w.number);
-						}}
-					>
-						<p>Week {w.number}</p>
-						<p tw="italic font-thin">{w.title}</p>
-					</MenuSelector>
-				))}
-			</ul>
-			<header>
-				<h1>Select Cabin</h1>
-			</header>
-			<ul tw="flex flex-wrap gap-1 justify-center">
-				{cabins.map((c) => (
-					<MenuSelector
-						tw="p-2 border w-1/5"
-						isSelected={c.name === selected.cabin}
-						onClick={() => select("cabin", c.name)}
-					>
-						{c.name}
-					</MenuSelector>
-				))}
-			</ul>
 		</>
 	);
 };
