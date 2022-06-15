24091723d3f86712a166d62bbe520f7f336556a0
knapptr <knapptr@gmail.com>
2022-06-15 16:36:40 -0400

    adds button styles

--- frontend/src/pages/CabinListIndex.js
+++ frontend/src/pages/CabinListIndex.js
@@ -12,9 +12,12 @@ const CabinListIndex = ()=>{
   });
   return (
     <>
+      <header><h2 tw="text-lg font-bold">Select Week</h2></header>
+      <div tw="flex gap-2 justify-center">
     {weeks && weeks.map(w=>{
-      return <Link to={`${w.number}`}><button tw="p-2">{w.number}</button></Link>
-    }) }
+      return <Link to={`${w.number}`}><button tw="p-2 bg-gray-300">{w.number}</button></Link>
+      }) }
+      </div>
       <Outlet/>
     </>
   )
