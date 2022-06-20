6d99fd4d16c3075e31a6c0d3bca3052d32a47957
knapptr <knapptr@gmail.com>
2022-06-20 17:52:01 -0400

    protects action route

--- server/routers/action.js
+++ server/routers/action.js
@@ -1,7 +1,9 @@
 const actionController = require('../handlers/action');
+const passport = require('passport');
 
 const router = require('express').Router();
 
+router.use(passport.authenticate("jwt", { session: false }));
 router.get("/",(req,res,next)=>{
   res.send('working');
 })
