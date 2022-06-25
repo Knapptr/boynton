69241015f37f04bd8e67e6a74a9bce1491e99c1e
Tyler Knapp <knapptr@gmail.com>
2022-06-25 08:25:48 -0400

    logs config on openhome

--- slack/listeners/events/openHome.js
+++ slack/listeners/events/openHome.js
@@ -1,6 +1,7 @@
 const homeView = require("../../views/home");
 const openHome = async ({ client, payload, event }) => {
 	console.log("opened home");
+  console.log(JSON.parse( process.env.CONFIG ))
 	client.views.publish({
 		user_id: event.user,
 		view: homeView,
