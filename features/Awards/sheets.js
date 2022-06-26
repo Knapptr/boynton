4df08a4675a9a40ccdd953ecd7e1303cf9d6ad2b
Tyler Knapp <knapptr@gmail.com>
2022-06-25 20:12:02 -0400

    Heroku woes

--- features/Awards/sheets.js
+++ features/Awards/sheets.js
@@ -2,7 +2,7 @@ const { GoogleSpreadsheet } = require("google-spreadsheet");
 const { formatmmddyyyy, getWeek, getYear } = require("../../utils/getDates");
 const sheetID = process.env.AWARD_SHEET_ID;
 
-console.log({private_key})
+console.log({private_key_before: process.env.GOOGLE_PRIVATE_KEY})
 	const private_key = process.env.GOOGLE_PRIVATE_KEY.replace(/\\\\n/g,"\n")
 console.log({private_key})
 
