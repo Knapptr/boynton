30a17e6581fccc05681311c41f8b996dc2d7c8fe
knapptr <knapptr@gmail.com>
2023-06-07 10:20:02 -0400

    Collapse menu attendance

--- frontend/src/pages/AttendanceIndex.js
+++ frontend/src/pages/AttendanceIndex.js
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import useGetDataOnMount from "../hooks/useGetData";
 import { MenuSelector } from "../components/styled";
 import toTitleCase from "../toTitleCase";
 import tw, { styled } from 'twin.macro';
 import 'styled-components/macro'
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const allHaveBeenSelected = (selected) => {
   if (
@@ -18,6 +22,8 @@ const allHaveBeenSelected = (selected) => {
 };
 
 const AttendanceIndex = () => {
+  const location = useLocation();
+  console.log({ attendanceLocationMatch: location.pathname === "/schedule/attendance" });
   const [schedule, setSchedule] = useGetDataOnMount({
     url: "/api/weeks",
     useToken: true,
@@ -47,62 +53,86 @@ const AttendanceIndex = () => {
     }
   }, [selected, schedule, navigate]);
 
+  const [showAccordion, setShowAccordion] = useState(false);
+
+  const handleAccordionChange = (e, state) => {
+    setShowAccordion(state);
+  }
   return (
     <>
       <h1 tw="text-xl font-bold">Attendance</h1>
-      <div tw="flex flex-wrap">
-        <ul tw="flex justify-center gap-3">
-          {schedule.map((week, weekIndex) => (
-            <MenuSelector
-              tw="px-4"
-              onClick={() => selectWeek(weekIndex)}
-              isSelected={selected.week === weekIndex}
-            >
-              <button>{week.number}</button>
-            </MenuSelector>
-          ))}
-        </ul>
-        {selected.week !== "none" &&
-          (
-            <>
-              <h1>Select Day</h1>
-              <ul tw="flex gap-3 justify-center">
-                {
-                  schedule[selected.week].days.map((day, dayIndex) => (
-                    <MenuSelector
-                      isSelected={selected.day === dayIndex}
-                      onClick={() => {
-                        selectDay(dayIndex);
-                      }}
-                    >
-                      <button>{toTitleCase(day.name)}</button>
-                    </MenuSelector>
-                  ))}
+      <Accordion tw="mb-6 shadow-none py-2 px-1 rounded bg-green-200" expanded={location.pathname === "/schedule/attendance" || showAccordion} onChange={handleAccordionChange}>
+        <AccordionSummary
+          expandIcon={location.pathname === "/schedule/attendance" ? <></> : <ExpandMoreIcon />}
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <p tw="text-center w-full font-bold">Schedule Selection</p>
+        </AccordionSummary>
+        <AccordionDetails>
+          <div tw="flex flex-col items-center gap-2">
+            <div tw="flex gap-2 items-center">
+              <h2>Week</h2>
+              <ul tw="flex justify-center gap-1" >
+                {schedule.map((week, weekIndex) => (
+                  <MenuSelector
+                    tw="px-4"
+                    onClick={() => selectWeek(weekIndex)}
+                    isSelected={selected.week === weekIndex}
+                  >
+                    <button>{week.number}</button>
+                  </MenuSelector>
+                ))}
               </ul>
-            </>
-          )}
-        {selected.day !== "none" &&
-          (
-            <>
-              <h1>Select Period</h1>
-              <ul tw="flex justify-center gap-3">
-                {
-                  schedule[selected.week].days[selected.day].periods.map(
-                    (period, periodIndex) => (
-                      <MenuSelector
-                        onClick={() => {
-                          selectPeriod(periodIndex);
-                        }}
-                        isSelected={periodIndex === selected.period}
-                      >
-                        <button>{period.number}</button>
-                      </MenuSelector>
-                    ))
-                }
-              </ul>
-            </>
-          )}
-      </div>
+            </div>
+            {selected.week !== "none" &&
+              (
+                <>
+
+                  <div tw="flex gap-2 items-center">
+                    <h2 tw="inline">Day</h2>
+                    <ul tw="flex gap-1 justify-center">
+                      {
+                        schedule[selected.week].days.map((day, dayIndex) => (
+                          <MenuSelector
+                            isSelected={selected.day === dayIndex}
+                            onClick={() => {
+                              selectDay(dayIndex);
+                            }}
+                          >
+                            <button>{toTitleCase(day.name)}</button>
+                          </MenuSelector>
+                        ))}
+                    </ul>
+                  </div>
+                </>
+              )}
+            {selected.day !== "none" &&
+              (
+                <>
+                  <div tw="flex gap-2 justify-center">
+                    <h2>Period</h2>
+                    <ul tw="flex justify-center gap-2">
+                      {
+                        schedule[selected.week].days[selected.day].periods.map(
+                          (period, periodIndex) => (
+                            <MenuSelector
+                              onClick={() => {
+                                selectPeriod(periodIndex);
+                              }}
+                              isSelected={periodIndex === selected.period}
+                            >
+                              <button>{period.number}</button>
+                            </MenuSelector>
+                          ))
+                      }
+                    </ul>
+                  </div>
+                </>
+              )}
+          </div>
+        </AccordionDetails>
+      </Accordion>
     </>
   );
 };
