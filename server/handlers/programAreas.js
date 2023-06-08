6bdd10b6caf6a11481e5ddc5821dc25c5299b94d
Tyler Knapp <knapptr@gmail.com>
2023-06-08 10:49:05 -0400

    Adds program area validation

--- server/handlers/programAreas.js
+++ server/handlers/programAreas.js
@@ -1,13 +1,19 @@
+const { body } = require("express-validator");
 const ProgramArea = require("../../models/programArea");
+const handleValidation = require("../../validation/validationMiddleware");
 module.exports = {
   getAll: async (req, res, next) => {
     const programAreas = await ProgramArea.getAll();
     res.json(programAreas);
   },
 
-  create: async (req, res, next) => {
-    const { areas } = req.body;
-    const createdAreas = await ProgramArea.create(areas);
-    res.json(createdAreas);
-  }
+  create: [
+    body("areas").isArray(),
+    body("areas.*.name").exists().trim().notEmpty(),
+    handleValidation,
+    async (req, res, next) => {
+      const { areas } = req.body;
+      const createdAreas = await ProgramArea.create(areas);
+      res.json(createdAreas);
+    }]
 }
