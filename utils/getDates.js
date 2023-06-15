db9ec05909970e43789ce62aff50cd95a3b56c9d
Tyler Knapp <knapptr@gmail.com>
2023-06-15 11:36:50 -0400

    remove json config

--- utils/getDates.js
+++ utils/getDates.js
@@ -1,33 +1,33 @@
-const { weeks } = JSON.parse(process.env.CONFIG);
-const getYear = () => {
-	return new Date().getFullYear();
-};
+// const { weeks } = JSON.parse(process.env.CONFIG);
+// const getYear = () => {
+// 	return new Date().getFullYear();
+// };
 
-const getWeek = (dateArg) => {
-	const date = dateArg ? Date.parse(dateArg) : new Date();
-	const keys = Object.keys(weeks);
-	const week = keys.find((key) => {
-		const start = Date.parse(weeks[key].start);
-		const end = Date.parse(weeks[key].end);
-		// console.log({ date, keys, start, end })
-		if (date >= start && date <= end) {
-			return true;
-		} else {
-			return false;
-		}
-	});
-	// console.log({week})
-	return week || "non";
-};
-const formatmmddyyyy = (date) => {
-	const mm = date.getMonth() + 1;
-	const dd = date.getDate();
-	const yyyy = date.getFullYear();
-	return `${mm}/${dd}/${yyyy}`;
-};
+// const getWeek = (dateArg) => {
+// 	const date = dateArg ? Date.parse(dateArg) : new Date();
+// 	const keys = Object.keys(weeks);
+// 	const week = keys.find((key) => {
+// 		const start = Date.parse(weeks[key].start);
+// 		const end = Date.parse(weeks[key].end);
+// 		// console.log({ date, keys, start, end })
+// 		if (date >= start && date <= end) {
+// 			return true;
+// 		} else {
+// 			return false;
+// 		}
+// 	});
+// 	// console.log({week})
+// 	return week || "non";
+// };
+// const formatmmddyyyy = (date) => {
+// 	const mm = date.getMonth() + 1;
+// 	const dd = date.getDate();
+// 	const yyyy = date.getFullYear();
+// 	return `${mm}/${dd}/${yyyy}`;
+// };
 
-const getTodayFormatted = () => {
-	return formatmmddyyyy(new Date());
-};
+// const getTodayFormatted = () => {
+// 	return formatmmddyyyy(new Date());
+// };
 
-module.exports = { getWeek, getYear, formatmmddyyyy, getTodayFormatted };
+// module.exports = { getWeek, getYear, formatmmddyyyy, getTodayFormatted };
