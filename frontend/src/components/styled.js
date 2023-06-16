7b34c379e072abad697b5b29ddad61bd0fcf1f44
Tyler Knapp <knapptr@gmail.com>
2023-06-15 21:16:33 -0400

    restores files

--- frontend/src/components/styled.js
+++
@@ -1,7 +0,0 @@
-import { Chip, styled } from "@mui/material";
-
-export const StaffBadge = styled(Chip)(({theme,type, firstYear, senior, ropes, archery, lifeguard }) => ({
-  color: theme.palette.badges[type],
-  fontWeight: "bold",
-}))
-
