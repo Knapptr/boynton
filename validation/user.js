e9a56f590d7bb4b626239a2a0d54d3a954218197
Tyler Knapp <knapptr@gmail.com>
2023-06-27 06:33:02 -0400

    REmove alphanumeric validation from username

--- validation/user.js
+++ validation/user.js
@@ -2,7 +2,7 @@ const { body, param } = require("express-validator");
 const pool = require("../db");
 const User = require("../models/User");
 const userValidation = {
-	validateUsername: () => body("username").trim().notEmpty().withMessage("username can not be blank").isAlphanumeric().withMessage("Username must be alphanumeric. No spaces.").isLength({ max: 24 }).withMessage("username too long. 24 characters max"),
+	validateUsername: () => body("username").trim().notEmpty().withMessage("username can not be blank").isLength({ max: 24 }).withMessage("username too long. 24 characters max"),
 
 	validateNameField: (nameField) => body(nameField).trim().notEmpty().escape().withMessage(`Invalid name field: ${nameField}`),
 
