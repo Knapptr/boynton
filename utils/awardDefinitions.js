d36486c6200d0946febd297096e16ba8c3875cf4
Tyler Knapp <knapptr@gmail.com>
2023-07-07 13:31:48 -0400

    fix awards

--- utils/awardDefinitions.js
+++ utils/awardDefinitions.js
@@ -1,9 +1,9 @@
 const AWARDS = [
   { awardType: "GENERIC", template: "genericAward_template.pptx" },
-  {
-    awardType: "Challenge Activities",
-    template: "challengeActivitiesAward_template.pptx",
-  },
+  // {
+  //   awardType: "Challenge Activities",
+  //   template: "challengeActivitiesAward_template.pptx",
+  // },
   // {awardType: "Waterfront", template: "waterfrontAward_template.pptx"},
 ];
 const getAwardTemplate = (programAreaString) => {
