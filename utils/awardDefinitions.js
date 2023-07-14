6e56a084d3a78fce3d9ad9111b72284414b59cf1
Tyler Knapp <knapptr@gmail.com>
2023-07-14 08:37:52 -0400

    make specific award templates

--- utils/awardDefinitions.js
+++ utils/awardDefinitions.js
@@ -1,13 +1,38 @@
 const AWARDS = [
   { awardType: "GENERIC", template: "genericAward_template.pptx" },
-  // {
-  //   awardType: "Challenge Activities",
-  //   template: "challengeActivitiesAward_template.pptx",
-  // },
-  // {awardType: "Waterfront", template: "waterfrontAward_template.pptx"},
+  {
+    awardType: "Challenge Activities",
+    template: "challengeactivities_template.pptx",
+  },
+  {awardType: "Waterfront",
+    template: "waterfront_template.pptx"},
+  {
+    awardType: "Creative Arts",
+    template: "creativearts_template.pptx",
+  },
+  {
+    awardType: "Archery",
+    template: "archery_template.pptx",
+  },
+  {
+    awardType: "Ropes",
+    template: "ropes_template.pptx",
+  },
+  {
+    awardType: "Superstar",
+    template: "superstar_template.pptx",
+  },
+  {
+    awardType: "Polar Bear Dip",
+    template: "pbd_template.pptx",
+  },
+  {
+    awardType: "Clean Cabin",
+    template: "cleancabin_template.pptx"
+  }
 ];
 const getAwardTemplate = (programAreaString) => {
-  const templates = [/*"Challenge Activities"*/];
+  const templates = ["Challenge Activities", "Waterfront", "Creative Arts","Archery","Ropes","Superstar","Polar Bear Dip", "Clean Cabin"];
   if (templates.includes(programAreaString)) {
     return programAreaString;
   } else {
