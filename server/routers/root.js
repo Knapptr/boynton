ed9217abb184937bee0d37be885ba787835a568b
Tyler Knapp <knapptr@gmail.com>
2025-06-20 12:54:25 -0400

    Adds health Check Route

--- server/routers/root.js
+++ server/routers/root.js
@@ -4,4 +4,5 @@ const path = require("path");
 router.get("/", (req, res) => {
 	res.sendFile(path.join(__dirname, "../../documentation/main.html"))
 })
+router.get("/health", (req,res) => res.send("OK"))
 module.exports = router;
